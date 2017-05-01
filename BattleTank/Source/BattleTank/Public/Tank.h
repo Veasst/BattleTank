befd7e66125909107c957b1a957e2dcf82c53596
Adrian Szczerbiński <veasst95@gmail.com>
2017-05-01 22:41:22 +0200

    Add IntendMoveForward method to TankMovementComponent

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -9,6 +9,7 @@ class UTankBarrel;
 class UTankTurret;
 class UTankAimingComponent;
 class AProjectile;
+class UTankMovementComponent;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
@@ -35,6 +36,9 @@ protected:
 	virtual void BeginPlay() override;
 
 	UTankAimingComponent* TankAimingComponent = nullptr;
+
+	UPROPERTY(BlueprintReadOnly)
+	UTankMovementComponent* TankMovementComponent = nullptr;
 
 private:
 	UPROPERTY(EditDefaultsOnly, Category = Setup)
